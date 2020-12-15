78d8aae5a40d70df9e6b53c44458f3f5aacff302
OenderTuetuenci <oender.tuetuenci@gmx.de>
2020-12-15 10:29:08 +0100

    Finished Server Started with Client

--- peerToPeerGroupChat/Client.py
+++ peerToPeerGroupChat/Client.py
@@ -1,20 +1,63 @@
 import socket
-from struct import pack,unpack
+from struct import pack, unpack
+import threading
+import time
 
 SERVER_IP = '127.0.0.1'
-SERVER_PORT = 50000
+SERVER_PORT = 50001
 
-sct = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sct.connect((SERVER_IP,SERVER_PORT))
+UDP_Port = 25557
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind(('', UDP_Port))
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((SERVER_IP, SERVER_PORT))
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind(('', UDP_Port))
+
+run = True
+# Registering
+succes = False
+
+while not succes:
+    nickname = input("Nickname?")
+    msg = pack("c30sI", b'r', nickname.encode(), UDP_Port)
+    server.send(msg)
+    data = server.recv(1024)
+    answer = data[1]
+    if answer == 1:
+        succes = True
+    else:
+        msg = unpack("c?I17s", data)
+        print(msg[3].decode("utf-8"))
+
+
+def listenServer():
+    global run
+    while run:
+        try:
+            data = server.recv(10000)
+            msg = unpack("cI", data[0:8])
+            length = msg[1]
+            msg = unpack("cI"+str(length)+"s",data)
+            clientList(msg[2])
+        except socket.timeout:
+            print("Timeout")
+
+def clientList(list):
+    list = list.decode("utf-8")
+    clients = list.split(";")
+    for client in clients:
+        if len(client) > 0:
+            attr = client.split(",")
+            print(attr[0]," UDP-Port: ", attr[1] + "," + attr[2])
+
+
+listenServer = threading.Thread(target=listenServer).start()
 
 x = ""
-msg = pack("c30sI",b'r',b'Matthias222',25525)
-print(msg)
-print(len(msg))
-sct.send(msg)
-
-msg = pack("c",b'd')
-sct.send(msg)
-
 while x != "q":
     x = input()
+run = False
--- peerToPeerGroupChat/Server.py
+++ peerToPeerGroupChat/Server.py
@@ -6,58 +6,96 @@ from threading import Thread
 
 class connectionThread(Thread):
     global nicKToAddr
-
+    global server
     def __init__(self, conn, addr):
         Thread.__init__(self)
         self.conn = conn
         self.addr = addr
+        self.nickname = ""
 
     def run(self):
-        while True:
+        while server:
             try:
                 data = self.conn.recv(1024)
                 msg = chr(data[0])
                 print(msg)
                 if msg == 'r':
-                    register(data, self.addr)
+                    register(data, self.addr,self)
                 elif msg == 'd':
-                    print("deregistering")
-                    conn.close()
-                    deregister(self)
+                    deregister(self.conn,self.nickname)
                     break
             except socket.timeout:
                 print('Socket timed out at', time.asctime())
+            except ConnectionResetError:
+                deregister(self.conn,self.nickname)
+        print("ending Thread ",self.nickname)
+
+def inputThread():
+    q = ""
+    global server
+    while q != "q":
+        q = input()
+        if q == "i":
+            client_list_broadcast()
+    server = False
 
 
-def register(data, addr):
-    nickname = unpack("30s", data[1:31])
-    udpport = unpack("I", data[32:])
-    nicKToAddr[nickname] = (addr[0], udpport)
-    print(nicKToAddr[nickname])
+def register(data, addr,thread):
+    info = unpack("c30sI", data)
+    nickname = info[1].decode("utf-8")
+    thread.nickname = nickname
+    if nickname in nicKToAddr.keys():
+        string = "Nickname is taken"
+        answer = pack("c?I17s",b'a',False,17,string.encode())
+        conn.send(answer)
+    else:
+        nicKToAddr[nickname] = (addr[0], info[2])
+        print("registered ",info[1])
+        answer = pack("c?",b'a',True)
+        conn.send(answer)
+        client_list_broadcast()
 
 
 
-def deregister(thread):
-    connList.remove(thread)
+def deregister(conn,nickname):
+    connList.remove(conn)
+    conn.close()
+    print("deregistering",nickname)
+    client_list_broadcast()
+
+def client_list_broadcast():
+    #omega String bauen
+    string=""
+    for nickname,port in nicKToAddr.items():
+        string = string + nickname + "," + str(port) + ";"
+    for conn in connList:
+        length = len(string)
+        msg = pack("cI"+str(length)+"s",b'c',length,string.encode())
+        conn.send(msg)
+    print("Sended Clientlist Broadcast")
+
 
 
 MY_IP = '127.0.0.1'
-MY_PORT = 50000
+MY_PORT = 50001
+server = True
 
 sct = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sct.bind((MY_IP, MY_PORT))
+sct.settimeout(10)
 print('Listening on Port ', MY_PORT, ' for incoming TCP connections')
 sct.listen(5)
 
 connList = []
 nicKToAddr = {}
 
-while True:
+inputThread = Thread(target=inputThread).start()
+
+while server:
     try:
         conn, addr = sct.accept()
         thread = connectionThread(conn, addr).start()
-        connList.append(thread)
+        connList.append(conn)
     except socket.timeout:
         print('Socket timed out listening', time.asctime())
-
 sct.close()
